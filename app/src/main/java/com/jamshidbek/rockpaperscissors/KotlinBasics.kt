60534e83e77785939fc55cc6b9f8bc803aef09ff
AkJames0114 <axmedovj0114@gmail.com>
2024-12-31 17:09:10 +0900

    RockPaperScissor.day2

---
+++ app/src/main/java/com/jamshidbek/rockpaperscissors/KotlinBasics.kt
@@ -0,0 +1,11 @@
+package com.jamshidbek.rockpaperscissors
+
+fun main() {
+
+    var userInput = readln()
+    while (userInput == "1"){
+        println("While loop executed")
+        userInput = readln()
+    }
+    println("Loop is done!")
+}
