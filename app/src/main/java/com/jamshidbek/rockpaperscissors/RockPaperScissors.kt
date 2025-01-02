c1b541596329b34f1e79a656519a501e467018a0
AkJames0114 <axmedovj0114@gmail.com>
2025-01-02 16:54:13 +0900

    Invalid input detector loop

--- app/src/main/java/com/jamshidbek/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/jamshidbek/rockpaperscissors/RockPaperScissors.kt
@@ -1,13 +1,24 @@
 package com.jamshidbek.rockpaperscissors
 
 fun main(){
+
     var computerChoice = ""
     var playerChoice = ""
-    println("Rock, Paper or Scissors? Enter your choice")
-    playerChoice = readln()
 
+    // Ask for the player's choice
+    println("Rock, Paper or Scissors? Enter your choice")
+    playerChoice = readln().lowercase() //Normalize player input to lowercase
+
+    // Input validation loop
+    while (playerChoice !in listOf("rock", "paper", "scissors")) {
+        println("Invalid choice. Please enter Rock, Paper, or Scissors.")
+        playerChoice = readln().lowercase()  // Ensure the input is normalized to lowercase
+    }
+
+    // Generate a random number to determine the computer's choice to lowercase
     val randomNumber = (1..3).random()
 
+    // Assign computer's choices
     when (randomNumber) {
         1 -> {
             computerChoice = "Rock"
@@ -19,17 +30,19 @@ fun main(){
             computerChoice = "Scissors"
         }
     }
+    // Display computer's choice
     println(computerChoice)
     
-
+    // Determine the winner!
     val winner = when {
         playerChoice == computerChoice -> "Tie"
-        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
-        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
-        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        playerChoice == "rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "scissors" && computerChoice == "Paper" -> "Player"
         else -> "Computer"
     }
 
+    // Output the results
     if (winner == "Tie"){
         println("It is a Tie!")
     }else{
