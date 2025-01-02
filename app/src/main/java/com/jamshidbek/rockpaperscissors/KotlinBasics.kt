0519f59494bae78d2319b40313ebb59cd983d3dc
AkJames0114 <axmedovj0114@gmail.com>
2025-01-02 17:40:21 +0900

    Amy's Coffee Machine.1

--- app/src/main/java/com/jamshidbek/rockpaperscissors/KotlinBasics.kt
+++ app/src/main/java/com/jamshidbek/rockpaperscissors/KotlinBasics.kt
@@ -1,11 +1,26 @@
 package com.jamshidbek.rockpaperscissors
 
-fun main() {
+fun main(){
 
-    var userInput = readln()
-    while (userInput == "1"){
-        println("While loop executed")
-        userInput = readln()
+    println("Who is this coffee for?")
+    var name = readln()
+
+    println("How many spoon of sugar do you want?")
+    val sugarCount = readln()
+
+    val sugarCountInt = sugarCount.toInt()
+    //Call Function
+    makeCoffee(sugarCountInt, name)
+
+}
+
+// Define Function SC
+fun makeCoffee(sugarCount:Int, name:String){
+    if (sugarCount == 1){
+        println("Coffee with $sugarCount spoon of sugar for $name is ready!")
+    }else if(sugarCount == 0){
+        println("Coffee with no sugar for $name is ready!")
+    }else{
+        println("Coffee with $sugarCount spoons of sugar for $name is ready!")
     }
-    println("Loop is done!")
 }
--- app/src/main/java/com/jamshidbek/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/jamshidbek/rockpaperscissors/RockPaperScissors.kt
@@ -50,3 +50,13 @@ fun main(){
     }
 
 }
+
+//fun main() {
+//
+//    var userInput = readln()
+//    while (userInput == "1"){
+//        println("While loop executed")
+//        userInput = readln()
+//    }
+//    println("Loop is done!")
+//}
